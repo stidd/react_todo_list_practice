441083149b47806be5cb99b6be8c8e427c97efeb
Steven Tidd <steven.tidd@gmail.com>
2019-03-31 16:35:10 -0400

    set up add to do list

--- src/index.js
+++ src/index.js
@@ -2,16 +2,38 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 class App extends Component {
-  state = {};
+  state = {
+    todos: [],
+    currentTodo: ""
+  };
+
+  handleToDo = e => {
+    this.setState({
+      currentTodo: e.target.value
+    });
+  };
+
+  handleSubmit = () => {
+    let todosCopy = this.state.todos.slice();
+    let newTodo = todosCopy.push(this.state.currentTodo);
+    this.setState({
+      todos: newTodo,
+      currentTodo: " "
+    });
+  };
 
   render() {
     return (
       <div>
         <h3>Keep Track of Your Daily Tasks Here</h3>
-        <form type="submit">
-          <input type="text" placeholder="type your today here" />
-          <button>Submit</button>
-        </form>
+        <input
+          type="text"
+          placeholder="type your today here"
+          onChange={this.handleToDo}
+        />
+        <button onClick={this.handleSubmit}>Submit</button>
+        <br />
+        {this.state.todos.length === 0 ? "no stuff" : "stuff"}
       </div>
     );
   }
