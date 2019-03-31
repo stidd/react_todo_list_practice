d5ff0578ecd3059976c0057bc5e8d7b201a1cc9e
Steven Tidd <steven.tidd@gmail.com>
2019-03-31 17:09:05 -0400

    make form a sfc

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
+function UserFormInput(props) {
+  return (
+    <form onSubmit={props.submit}>
+      <h3>Keep Track of Your Daily Tasks Here</h3>
+      <input
+        type="text"
+        placeholder="type your today here"
+        onChange={props.todo}
+      />
+      <button>Submit</button>
+    </form>
+  );
+}
+
 class App extends Component {
   state = {
     todos: [],
@@ -8,12 +22,15 @@ class App extends Component {
   };
 
   handleToDo = e => {
+    console.log(e.target.value);
     this.setState({
       currentTodo: e.target.value
     });
   };
 
-  handleSubmit = () => {
+  handleSubmit = e => {
+    e.preventDefault();
+
     let todosCopy = this.state.todos.slice();
     let newTodo = todosCopy.push(this.state.currentTodo);
     this.setState({
@@ -25,15 +42,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h3>Keep Track of Your Daily Tasks Here</h3>
-        <input
-          type="text"
-          placeholder="type your today here"
-          onChange={this.handleToDo}
-        />
-        <button onClick={this.handleSubmit}>Submit</button>
-        <br />
-        {this.state.todos.length === 0 ? "no stuff" : "stuff"}
+        <UserFormInput submit={this.handleSubmit} todo={this.handleToDo} />
+        {this.state.todos.length === 0 ? "notta" : "something"}
       </div>
     );
   }
